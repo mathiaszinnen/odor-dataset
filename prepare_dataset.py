dc020727755b78eb45d2a016c647c735d1f1d587
Mathias Zinnen~ <mathias.zinnen@fau.de>
2024-04-26 14:27:01 +0200

    add dataset preparation script

---
+++ prepare_dataset.py
@@ -0,0 +1,40 @@
+import wget, zipfile
+import os
+
+
+def prepare_folders():
+    if not os.path.isdir('data'):
+        os.makedirs('data')
+
+def download_dataset():
+    dataset_url = 'https://zenodo.org/api/records/11070878/files-archive'
+    fn = wget.download(dataset_url, out='data/', bar=None)
+    return fn
+
+def unzip(fn, target):
+    with zipfile.ZipFile(fn) as zf:
+        zf.extractall(target)
+
+
+def cleanup(fn):
+    os.remove(fn)
+    os.remove('data/images.zip')
+
+if __name__ == '__main__':
+    print('Preparing folders..')
+    prepare_folders()
+
+    print('Downloading dataset..')
+    fn = download_dataset()
+    
+    print('Unzipping dataset..')
+    unzip(fn, 'data/')
+
+    unzip('data/images.zip', 'data/images')
+
+    print('Removing temporary files')
+    cleanup(fn)
+
+    print('Dataset succesfully prepared in data/')
+
+
